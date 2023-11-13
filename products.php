f7165eb406e5329ec948e9b22962571521ff7549
Garuka Veenath Assalaarachchi <garukavassalaarachchi@gmail.com>
2023-11-13 22:39:20 +0530

    updated

---
+++ products.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
+</head>
+<body>
+    <nav class="sidebar close">
+        <header>
+            <div class="image-text">
+                <span class="image">
+                    <img src="../icons/white.png" alt="">
+                </span>
+
+                <div class="text logo-text">
+                    <span class="name">Admin Panel</span>
+                </div>
+            </div>
+            <i class='bx bx-menu toggle'></i>
+        </header>
+
+        <div class="menu-bar">
+            <div class="menu">
+                <ul class="menu-links">
+                    <li class="nav-link">
+                        <a href="index.php">
+                            <i class='bx bx-home-alt icon' ></i>
+                            <span class="text nav-text">Dashboard</span>
+                        </a>
+                    </li>
+
+                    <li class="nav-link">
+                        <a href="products.php">
+                            <i class='bx bx-box icon' ></i>
+                            <span class="text nav-text">Products</span>
+                        </a>
+                    </li>
+
+                    <li class="nav-link">
+                        <a href="categories.php">
+                        <i class='bx bx-category icon' ></i>
+                            <span class="text nav-text">Categories</span>
+                        </a>
+                    </li>
+
+                    <li class="nav-link">
+                        <a href="orders.php">
+                            <i class='bx bx-list-ul icon' ></i>
+                            <span class="text nav-text">Order list</span>
+                        </a>
+                    </li>
+                    
+                    <li class="nav-link">
+                        <a href="users.php">
+                        <i class='bx bx-user icon' ></i>
+                            <span class="text nav-text">Users</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+ 
+            <div class="bottom-content">
+                <li class="">
+                    <a href="#">
+                        <i class='bx bx-log-out icon' ></i>
+                        <span class="text nav-text">Logout</span>
+                    </a>
+                </li>
+            </div>
+        </div>
+
+    </nav>
+
+    <section class="home">
+        <div class="header"> Products</div>
+    </section>
+
+    <script>
+        const body = document.querySelector('body'),
+      sidebar = body.querySelector('nav'),
+      toggle = body.querySelector(".toggle");
+
+
+toggle.addEventListener("click" , () =>{
+    sidebar.classList.toggle("close");
+})
+    </script>
+
+</body>
+</html>
