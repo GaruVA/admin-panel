ad6322b76152ff7d77daa01c7655553fb340f831
Garuka Veenath Assalaarachchi <garukavassalaarachchi@gmail.com>
2023-11-20 12:38:27 +0530

    CSS

--- products.php
+++ products.php
@@ -11,7 +11,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script> 
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"></script>
-    <link rel="stylesheet" href="categories.css">
+    <link rel="stylesheet" href="products.css">
 </head>
 <body>
     <nav class="sidebar close">
